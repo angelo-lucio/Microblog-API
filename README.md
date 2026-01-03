effaad7b6055e088364e6add5c21f851beaeb7b5
kokilan98 <kokilanrajaratnam@gmail.com>
2026-01-03 12:18:42 +0100

    db added

--- README.md
+++ README.md
@@ -1,15 +1,28 @@
 # microblog-api
 
-To install dependencies:
+## To install dependencies:
 
 ```bash
 bun install
 ```
 
-To run:
+## Setting up PostgreSQL
+
+You can quickly run a PostgreSQL container for development using Docker:
+```bash
+docker run --name my-postgres -p 5432:5432 \
+  -e POSTGRES_PASSWORD=supersecret123 \
+  -e POSTGRES_DB=minitwitter \
+  -d postgres
+```
+
+
+## To run:
 
 ```bash
 bun start dev
 ```
+
+## Kokilan dont forget to update readme for db schame migration
 
 This project was created using `bun init` in bun v1.3.1. [Bun](https://bun.com) is a fast all-in-one JavaScript runtime.
--- src/api.ts
+++ src/api.ts
@@ -1,57 +1,36 @@
 import express, { type Express, type Request, type Response } from "express";
+import { postsTable } from "./db/schema";
+import { db } from "./database";
+import { eq } from "drizzle-orm";
 
 export const initializeAPI = (app: Express) => {
-    let posts = [
-        { id: 1, content: "I feel like I am a post" },
-        { id: 2, content: "Today is a good day" },
-        { id: 3, content: "I have a lot of posts" },
-        { id: 4, content: "My posts are the best" },
-    ];
 
     app.get("/hello-world", (req: Request, res: Response) => {
         res.send("Hello World!");
     });
 
-    app.get('/posts', (req: Request, res: Response) => {
-        res.send(posts)
-    })
-
-    app.post("/posts", (req: Request, res: Response) => {
-        const newPost = {
-            id: posts.length + 1,
-            content: req.body.content
-        }
-        
-        // const newPost = req.body;
-        // why changed? typescript safty error. this code wotkd but typescript is warning
-        // newPost.id = posts[posts.length - 1].id + 1;
-
-        posts.push(newPost);
-        res.send(newPost);
+    app.get("/posts", async (req: Request, res: Response) => {
+        const posts = await db.select().from(postsTable);
+        res.send(posts);
     });
 
-    app.put("/posts/:id", (req: Request, res: Response) => {
+    app.post("/posts", async (req: Request, res: Response) => {
+        const newPost = await db.insert(postsTable).values(req.body)
+            .returning();
+        res.send(newPost[0]);
+    });
+
+    app.put("/posts/:id", async (req: Request, res: Response) => {
         const id = Number(req.params.id);
-        // if the param is not a numnber it will throw an error but number will handle it as NaN
-        // we can discuss how strict do we want
-        //const id = parseInt(req.params.id);
-        const updatedPost = req.body;
-        const existingPost = posts.find((post) => post.id === id);
-        if (!existingPost) {
-            res.status(404).send("Post not found");
-            return;
-        }
-        updatedPost.id = id;
-        posts = posts.map((post) => (post.id === id ? updatedPost : post));
-        res.send(updatedPost);
+        const updatedPost = await db.update(postsTable).set(req.body).where(
+            eq(postsTable.id, id),
+        ).returning();
+        res.send(updatedPost[0]);
     });
 
     app.delete("/posts/:id", (req: Request, res: Response) => {
         const id = Number(req.params.id);
-        // if the param is not a numnber it will throw an error but number will handle it as NaN
-        // we can discuss how strict do we want
-        //const id = parseInt(req.params.id);
-        posts = posts.filter((post) => post.id !== id);
-        res.send(posts);
+        db.delete(postsTable).where(eq(postsTable.id, id)).execute();
+        res.send({ id });
     });
 };
